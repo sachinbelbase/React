23031ac7902726e9089deb3ec845bb21c837a184
sachinbelbase <sachinbelbase78@gmail.com>
2026-06-14 15:55:21 +0545

    to i have learn how we can create and setup the git hub file

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+        <h1>Day 2 of Learning React</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Code with Sachin with vite </h1>
+  )
+}
+
+export default App

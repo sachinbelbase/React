9114fa99a1fe0695f6141ad0f25f0e4f8fa81a4e
sachinbelbase <sachinbelbase78@gmail.com>
2026-06-23 19:15:36 +0545

    add basic reduxtoolkit project

---
+++ 11reduxtoolkitTodo/src/App.jsx
@@ -0,0 +1,13 @@
+import Todos from "./components/Todos"
+import AddTodo from './components/AddTodo'
+
+function App() {
+  return (
+    <>
+      <h1>Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+export default App
